031a62ed34e673f662fd70f5c34aadb1afa774fc
yardenDanan <34451733+yardenDanan@users.noreply.github.com>
2018-06-14 22:34:52 +0300

    Omer
    New windows

--- CpsApplication/src/application/CasualOrderWindowController.java
+++ CpsApplication/src/application/CasualOrderWindowController.java
@@ -1,42 +1,25 @@
-/**
- * Sample Skeleton for 'CasualOrderWindow.fxml' Controller Class
- */
-
 package application;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import jfxtras.scene.control.CalendarTimeTextField;
 
 public class CasualOrderWindowController {
 
-    @FXML // fx:id="tf_CarNumber"
-    private TextField tf_CarNumber; // Value injected by FXMLLoader
-
-    @FXML // fx:id="tf_CustomerId"
-    private TextField tf_CustomerId; // Value injected by FXMLLoader
-
     @FXML // fx:id="tf_Email"
     private TextField tf_Email; // Value injected by FXMLLoader
 
-    @FXML // fx:id="tf_Date"
-    private DatePicker tf_Date; // Value injected by FXMLLoader
+    @FXML // fx:id="tf_LeavingDate"
+    private DatePicker tf_LeavingDate; // Value injected by FXMLLoader
 
-    @FXML // fx:id="tf_Time"
-    private CalendarTimeTextField tf_Time; // Value injected by FXMLLoader
+    @FXML // fx:id="tf_Id"
+    private TextField tf_Id; // Value injected by FXMLLoader
 
-    @FXML // fx:id="btn_Cancel"
-    private Button btn_Cancel; // Value injected by FXMLLoader
+    @FXML // fx:id="tf_LeavingTime"
+    private CalendarTimeTextField tf_LeavingTime; // Value injected by FXMLLoader
 
-    @FXML // fx:id="btn_MakeOrder"
-    private Button btn_MakeOrder; // Value injected by FXMLLoader
-
-    @FXML
-    void cancel(ActionEvent event) {
-
-    }
+    @FXML // fx:id="tf_CarNumber"
+    private TextField tf_CarNumber; // Value injected by FXMLLoader
 
 }
--- CpsApplication/src/application/OneTimePreOrderWindowController.java
+++ CpsApplication/src/application/OneTimePreOrderWindowController.java
@@ -1,6 +1,5 @@
 package application;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -10,39 +9,34 @@ import jfxtras.scene.control.CalendarTimeTextField;
 
 public class OneTimePreOrderWindowController {
 
-    @FXML // fx:id="tf_CarNumber"
-    private TextField tf_CarNumber; // Value injected by FXMLLoader
-
-    @FXML // fx:id="tf_CustomerId"
-    private TextField tf_CustomerId; // Value injected by FXMLLoader
-
     @FXML // fx:id="tf_Email"
     private TextField tf_Email; // Value injected by FXMLLoader
-
-    @FXML // fx:id="tf_ArriveDate"
-    private DatePicker tf_ArriveDate; // Value injected by FXMLLoader
-
-    @FXML // fx:id="tf_ArriveTime"
-    private CalendarTimeTextField tf_ArriveTime; // Value injected by FXMLLoader
-
-    @FXML // fx:id="tf_LeavingDate"
-    private DatePicker tf_LeavingDate; // Value injected by FXMLLoader
-
-    @FXML // fx:id="tf_LeavingTime"
-    private CalendarTimeTextField tf_LeavingTime; // Value injected by FXMLLoader
-
-    @FXML // fx:id="cb_Branch"
-    private ComboBox<?> cb_Branch; // Value injected by FXMLLoader
 
     @FXML // fx:id="btn_Cancel"
     private Button btn_Cancel; // Value injected by FXMLLoader
 
-    @FXML // fx:id="btn_MakeOrder"
-    private Button btn_MakeOrder; // Value injected by FXMLLoader
+    @FXML // fx:id="tf_LeavingDate"
+    private DatePicker tf_LeavingDate; // Value injected by FXMLLoader
 
-    @FXML
-    void cancel(ActionEvent event) {
+    @FXML // fx:id="tf_ArrivingDate"
+    private DatePicker tf_ArrivingDate; // Value injected by FXMLLoader
 
-    }
+    @FXML // fx:id="tf_Id"
+    private TextField tf_Id; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tf_LeavingTime"
+    private CalendarTimeTextField tf_LeavingTime; // Value injected by FXMLLoader
+
+    @FXML // fx:id="Btn_MakeOrder"
+    private Button Btn_MakeOrder; // Value injected by FXMLLoader
+
+    @FXML // fx:id="cb_Branch"
+    private ComboBox<?> cb_Branch; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tf_ArrivingTime"
+    private CalendarTimeTextField tf_ArrivingTime; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tf_CarNumber"
+    private TextField tf_CarNumber; // Value injected by FXMLLoader
 
 }
