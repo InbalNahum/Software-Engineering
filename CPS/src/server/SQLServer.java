4b7a884ea719c6a13e35284e6be0ba739171a193
yardenDanan <34451733+yardenDanan@users.noreply.github.com>
2018-06-22 17:33:38 +0300

    yarden

--- CPS/src/server/SQLServer.java
+++ CPS/src/server/SQLServer.java
@@ -1,7 +1,4 @@
 package server;
-// This file contains material supporting section 3.7 of the textbook:
-// "Object Oriented Software Engineering" and is issued under the open-source
-// license found at www.lloseng.com 
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -40,16 +37,6 @@ import parkingLogic.Car;
 import parkingLogic.Location;
 import parkingLogic.ParkingFloor;
 
-/**
- * This class overrides some of the methods in the abstract 
- * superclass in order to give more functionality to the server.
- *
- * @author Dr Timothy C. Lethbridge
- * @author Dr Robert Lagani&egrave;re
- * @author Fran&ccedil;ois B&eacute;langer
- * @author Paul Holden
- * @version July 2000
- */
 public class SQLServer extends AbstractServer 
 {
 
